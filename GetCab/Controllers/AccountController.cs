c15fb2af227f631fe74498b605a0d4fba289ce75
Andrey Lazarev <drafter221@gmail.com>
2018-03-30 20:37:33 +0400

    second

---
+++ GetCab/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using GetCab.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace GetCab.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = null;
+                using (var db = new UserContext())
+                {
+                    user = db.Users.FirstOrDefault(u => u.Email == model.Email);
+                }
+                if (user == null)
+                {
+                    using (UserContext db = new UserContext())
+                    {
+                        db.Users.Add(new User { Email = model.Email, Password = model.Password, FirstName = model.FirstName, LastName = model.LastName, Birthday = model.Birthday, Patronymic = model.Patronymic, Phone = model.Phone });
+                        db.SaveChanges();
+
+                        user = db.Users.Where(u => u.Email == model.Email && u.Password == model.Password).FirstOrDefault();
+                    }
+                    if (user != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(model.Email, true);
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "UZHE EST'");
+                    }
+                }
+            }
+            return View(model);
+        }
+
+
+
+    }
+}
---
+++ GetCab/Models/UserContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace GetCab.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() :
+            base("DegaultConnection")
+        { }
+
+        public DbSet<User> Users { get; set; }
+
+
+        public string Login { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
