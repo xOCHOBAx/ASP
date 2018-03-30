f2b06801b29dc93fdd15bc463e181a7af1c29766
awfly <a-nton.99@yandex.ru>
2018-03-30 21:55:01 +0400

    Dobavil Andrei.cs

---
+++ GetCab/Models/Andrei.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GetCab.Models
+{
+    public class Andrei
+    {
+        public string chlex = "hui";
+        int kek;
+    }
+}
